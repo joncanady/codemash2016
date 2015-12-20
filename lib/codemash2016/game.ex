3feb72ae1decf52ff4427512789031733ea19449
Jon Canady <jon@joncanady.com>
2015-12-20 08:34:49 -0500

    Games can be waiting for players

--- lib/codemash2016/game.ex
+++ lib/codemash2016/game.ex
@@ -12,4 +12,25 @@ defmodule Codemash2016.Game do
     |> String.slice(0, 5)
     |> String.upcase
   end
+
+  @doc"""
+  Returns true if the `game` is missing either player.
+  """
+  def waiting_for_players?(game) do
+    is_nil(player_one_name(game)) or is_nil(player_two_name(game))
+  end
+
+  @doc"""
+  Returns player one's name, or nil if player one hasn't joined.
+  """
+  def player_one_name(game) do
+    elem(game.player_one, 0)
+  end
+
+  @doc"""
+  Returns player two's name, or nil if player two hasn't joined.
+  """
+  def player_two_name(game) do
+    elem(game.player_two, 0)
+  end
 end
--- test/lib/codemash2016/game_test.exs
+++ test/lib/codemash2016/game_test.exs
@@ -1,7 +1,28 @@
 defmodule Codemash2016.GameTest do
   use ExUnit.Case, async: true
+  alias Codemash2016.Game
 
   test "generate_game_code returns a five-length String" do
-    assert Codemash2016.Game.generate_game_code |> String.length == 5
+    assert Game.generate_game_code |> String.length == 5
+  end
+
+  test "a game is waiting for players if either of the players are unnamed" do
+    game_missing_p1 = %Game{game_code: 'CODE',
+                            player_one: {nil, nil},
+                            player_two: {'Bob', nil}}
+    game_missing_p2 = %Game{game_code: 'CODE',
+                            player_one: {'Alice', nil},
+                            player_two: {nil, nil}}
+
+    assert Game.waiting_for_players?(game_missing_p1)
+    assert Game.waiting_for_players?(game_missing_p2)
+  end
+
+  test "a game is not waiting for players if both players are named" do
+    started_game = %Game{game_code: 'CODE',
+                         player_one: {'Alice', nil},
+                         player_two: {'Bob', nil}}
+
+    refute Game.waiting_for_players?(started_game)
   end
 end
